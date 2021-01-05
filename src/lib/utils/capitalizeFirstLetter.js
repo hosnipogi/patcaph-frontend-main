069d71417c1435db931a83832c80c9c9ba8a245c
hosnipogi <hosnibona@gmail.com>
2021-01-06 07:15:02 +0800

    Fixed bug causing fail build

--- src/lib/utils/capitalizeFirstLetter.js
+++ src/lib/utils/capitalizeFirstLetter.js
@@ -1,3 +1,5 @@
 export default function capitalizeFLetter(word) {
-  return word[0].toUpperCase() + word.slice(1)
+  return (
+    typeof window === "object" && `${word[0].toUpperCase()}${word.slice(1)}`
+  )
 }
