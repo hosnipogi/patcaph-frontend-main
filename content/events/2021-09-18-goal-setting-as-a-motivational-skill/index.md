31d578ec439d87ffd2b3fa15b78e28def3182d02
hosnipogi <hosnibona@gmail.com>
2021-09-18 15:12:52 +0800

    added an event

---
+++ content/events/2021-09-18-goal-setting-as-a-motivational-skill/index.md
@@ -0,0 +1,14 @@
+---
+slug: "/goal-setting-as-a-motivational-skill"
+event_date: "2021-09-28"
+event_time: "1:00PM - 3:00PM"
+title: "Goal Setting as a Motivational Skill"
+featuredImage:
+  alt: "Goal Setting as a Motivational Skill"
+  src: "./emeet.jpg"
+external_url: "https://us02web.zoom.us/meeting/register/tZIpde-vqDsiHt1E2-bSkU1gqjpw5CvZSH4k"
+---
+
+Click <a href="https://us02web.zoom.us/meeting/register/tZIpde-vqDsiHt1E2-bSkU1gqjpw5CvZSH4k" target="_blank" rel="noreferrer" style="color:blue;text-decoration:underline">here</a> to register.
+
+<!-- ![Goal Setting as a Motivational Skill](./emeet.jpg) -->
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types"
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
@@ -5,46 +6,68 @@ import SEO from "../../components/seo"
 
 const events = ({ data }) => {
   const events = data.allFile.edges
+  const dateToday = new Date().getTime()
+
+  const upcomingEvents = []
+  const pastEvents = []
+
+  events.forEach(({ node }) => {
+    const event_date = node.childMarkdownRemark.frontmatter.event_date
+    const eventDate = new Date(event_date).getTime()
+    if (eventDate > dateToday) {
+      upcomingEvents.push(node)
+    } else {
+      pastEvents.push(node)
+    }
+  })
+
+  return (
+    <>
+      <div className="mb-4">
+        <h3>Upcoming</h3>
+        <Posts events={upcomingEvents} />
+      </div>
+      <div>
+        <h3>Past Events</h3>
+        <Posts events={pastEvents} />
+      </div>
+    </>
+  )
+}
+
+const Posts = ({ events }) => {
   return (
     <>
       <SEO title="Events" />
       <div>
         <ul className="pl-5 my-4 list-decimal">
-          {events.map(
-            ({
-              node: {
-                childMarkdownRemark: { frontmatter, html },
-              },
-            }) => (
-              <li key={frontmatter.title} className="mb-4">
-                <div className="mb-4">
-                  <h4>{frontmatter.title}</h4>
-                  <p>
-                    {new Date(frontmatter.event_date).toDateString()}
-                    {", "}
-                    {frontmatter.event_time}
-                  </p>
-                  <div
-                    className="post-content"
-                    dangerouslySetInnerHTML={{ __html: html }}
+          {events.map(({ childMarkdownRemark: { frontmatter, html } }) => (
+            <li key={frontmatter.title} className="mb-4">
+              <div className="mb-4">
+                <h4>{frontmatter.title}</h4>
+                <p>
+                  {new Date(frontmatter.event_date).toDateString()}
+                  {", "}
+                  {frontmatter.event_time}
+                </p>
+                <div
+                  className="post-content"
+                  dangerouslySetInnerHTML={{ __html: html }}
+                />
+                <a
+                  href={frontmatter.external_url}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Img
+                    fluid={frontmatter.featuredImage.src.childImageSharp.fluid}
+                    className=""
                   />
-                  <a
-                    href={frontmatter.external_url}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <Img
-                      fluid={
-                        frontmatter.featuredImage.src.childImageSharp.fluid
-                      }
-                      className=""
-                    />
-                  </a>
-                </div>
-                <hr />
-              </li>
-            )
-          )}
+                </a>
+              </div>
+              <hr />
+            </li>
+          ))}
         </ul>
       </div>
     </>
@@ -88,3 +111,7 @@ export const eventsQuery = graphql`
     }
   }
 `
+
+Posts.propTypes = {
+  events: PropTypes.array,
+}
