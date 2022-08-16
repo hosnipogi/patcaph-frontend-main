f6ef3a00f05df9d4bc4e4a78099f45150bd38d80
hosnipogi <hosnibona@gmail.com>
2022-08-17 05:49:40 +0800

    aprm registration under construction

---
+++ src/pages/aprm-2022-registration.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Aprm = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        padding: "20px 0",
+      }}
+    >
+      <img
+        src="https://www.seekpng.com/png/detail/259-2598162_page-in-under-construction.png"
+        alt="Page In Under Construction@seekpng.com"
+        width="400px"
+        height="400px"
+        style={{
+          marginInline: "auto",
+        }}
+      />
+    </div>
+  )
+}
+
+export default Aprm
